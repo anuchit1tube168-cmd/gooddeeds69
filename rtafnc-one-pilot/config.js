35acc47319b220aedf1d210939006dda537269c1
AGISLAMIOUS <anuchit1tube168@gmail.com>
2026-08-29 22:16:50 +0700

    Standardize developer display and enable staff RBAC flag

--- rtafnc-one-pilot/config.js
+++ rtafnc-one-pilot/config.js
@@ -1,6 +1,7 @@
 window.RTAFNC_ONE_CONFIG = Object.freeze({
   APP_NAME: "RTAFNC ONE",
-  PILOT_VERSION: "0.4.0",
+  PILOT_VERSION: "0.4.1",
+  DEVELOPER_DISPLAY: "ผปค.วพอ.พอ. ร.อ.อนุชิต ทำจะดี ผู้พัฒนา",
   GOOD_DEED_API_URL: "https://script.google.com/macros/s/AKfycbwV0b31hWMSs2oNOff4o-O_PNoEQ1XlTM77f4sei9JLh1rza1SfFPTOlTaxiIKCIxLT_Q/exec",
   CURRENT_LIFF_ID: "2010948179-Ympqt2bT",
   CURRENT_LIFF_URL: "https://liff.line.me/2010948179-Ympqt2bT",
@@ -19,7 +20,9 @@ window.RTAFNC_ONE_CONFIG = Object.freeze({
     SECURE_SOURCE_OPEN: true,
     MCP_ACTIONS: false,
     AUTO_ACADEMIC_YEAR: true,
-    RESPONSIVE_RUNTIME: true
+    RESPONSIVE_RUNTIME: true,
+    STAFF_RBAC: true,
+    STAFF_PHOTOS: false
   }),
 
   LINKS: {
