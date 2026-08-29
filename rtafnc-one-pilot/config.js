79451bafd69ccc5ea9627e358ca1d77ac6f4df06
AGISLAMIOUS <anuchit1tube168@gmail.com>
2026-08-29 08:02:19 +0700

    feat(rtafnc-one): add isolated pilot config

---
+++ rtafnc-one-pilot/config.js
@@ -0,0 +1,17 @@
+window.RTAFNC_ONE_CONFIG = Object.freeze({
+  APP_NAME: "RTAFNC ONE",
+  PILOT_VERSION: "0.1.0",
+  GOOD_DEED_API_URL: "https://script.google.com/macros/s/AKfycbwV0b31hWMSs2oNOff4o-O_PNoEQ1XlTM77f4sei9JLh1rza1SfFPTOlTaxiIKCIxLT_Q/exec",
+  CURRENT_LIFF_ID: "2010948179-Ympqt2bT",
+  CURRENT_LIFF_URL: "https://liff.line.me/2010948179-Ympqt2bT",
+  REQUEST_TIMEOUT_MS: 30000,
+  RESPONSE_ORIGINS: ["https://script.google.com", "https://script.googleusercontent.com"],
+  LINKS: {
+    GOOD_DEED: "../frontend/student-dashboard.html",
+    APPROVAL: "../frontend/teacher-dashboard.html",
+    CARD_SHOWCASE: "../frontend/view_cards.html",
+    PROFILE: "../frontend/profile.html",
+    SIGN: "../frontend/approve_sign.html",
+    SUBMIT_DEED: "../frontend/submit-deed.html"
+  }
+});
