f0f8f27e195edb3d81b083d9b3c5654f9a6525bb
AGISLAMIOUS <anuchit1tube168@gmail.com>
2026-09-02 09:36:07 +0700

    phase3a: add fail-closed Cloudflare staging config

---
+++ frontend/cloudflare-staging/config.js
@@ -0,0 +1,9 @@
+window.GOOD_DEED_CLOUDFLARE = Object.freeze({
+  APP_NAME: "ระบบบันทึกความดี วพอ. 2569",
+  STAGING_LIFF_ID: "REPLACE-WITH-STAGING-LIFF-ID",
+  GATEWAY_BASE_URL: "",
+  MODULE_PATH: "/api/gooddeed",
+  PRODUCTION_CUTOVER: false,
+  WRITES_ENABLED: false,
+  SAME_ORIGIN_REQUIRED: true
+});
